91879492cef7867a4d799b719a6ece8a7a6400a3
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-23 12:44:20 +0300

    Implemented Basic changes in each files

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def first():
+    return app.send_static_file("Website.html")
+
+app.run(host = "localhost",port =5001,debug = True)
+##if __name__ == "__main__":
