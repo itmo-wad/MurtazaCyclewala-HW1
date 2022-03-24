c497aabd1fcc44de6718d49d468240db4b68935b
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-24 10:33:17 +0300

    Add a footer, changed folders, updated the code

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/Profile')
 def first():
-    return app.send_static_file("Website.html")
+    return render_template("Website.html")
 
 if __name__ == "__main__":
     app.run(host = "localhost",port =5001,debug = True)
