30740edabf1ce320b59432f7c66fdfc16c2a2109
Murtaza Cyclewala <80732148+MurtazaCyclewala@users.noreply.github.com>
2022-03-24 09:59:01 +0300

    HTML - completed most of frontend editings, remaining more profile data

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def first():
     return app.send_static_file("Website.html")
 
-app.run(host = "localhost",port =5001,debug = True)
-##if __name__ == "__main__":
+if __name__ == "__main__":
+    app.run(host = "localhost",port =5001,debug = True)
